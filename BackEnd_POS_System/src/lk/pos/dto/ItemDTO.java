236842a763963bea88817f05107b5669b02f5053
dhambure98 <akila789liyanage@gmail.com>
2022-07-15 15:18:12 +0530

    Update ItemDTO

--- BackEnd_POS_System/src/lk/pos/dto/ItemDTO.java
+++ BackEnd_POS_System/src/lk/pos/dto/ItemDTO.java
@@ -6,4 +6,60 @@ package lk.pos.dto;
  **/
 
 public class ItemDTO {
+    private String itemCode;
+    private String itemName;
+    private int qtyOnHand;
+    private double unitPrice;
+
+    public ItemDTO() {
+    }
+
+    public ItemDTO(String itemCode, String itemName, int qtyOnHand, double unitPrice) {
+        this.itemCode = itemCode;
+        this.itemName = itemName;
+        this.qtyOnHand = qtyOnHand;
+        this.unitPrice = unitPrice;
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getQtyOnHand() {
+        return qtyOnHand;
+    }
+
+    public void setQtyOnHand(int qtyOnHand) {
+        this.qtyOnHand = qtyOnHand;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemDTO{" +
+                "itemCode='" + itemCode + '\'' +
+                ", itemName='" + itemName + '\'' +
+                ", qtyOnHand=" + qtyOnHand +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
 }
