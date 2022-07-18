5df12b612d5aef3c47b957f70aa4d2657b838633
dhambure98 <akila789liyanage@gmail.com>
2022-07-18 12:40:12 +0530

    Add doGet methord for PlaseOrder and Update CORSFilter

--- BackEnd_POS_System/src/lk/pos/filters/CORSFilter.java
+++ BackEnd_POS_System/src/lk/pos/filters/CORSFilter.java
@@ -1,9 +1,19 @@
 package lk.pos.filters;
 
+import java.io.IOException;
+
+
 /**
  * @author : A.D.Liyanage
  * @since : 0.1.0
  **/
 
-public class CORSFilter {
+@WebServlet(urlPatterns = "/customerss")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("request ");
+    }
 }
+//http://localhost:8080/javaEEpos/customer
