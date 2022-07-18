16078ff16316a144e3a7bc5952efd6a9f95f485e
dhambure98 <akila789liyanage@gmail.com>
2022-07-18 12:47:46 +0530

    Update Servlet

--- BackEnd_POS_System/src/lk/pos/filters/CORSFilter.java
+++ BackEnd_POS_System/src/lk/pos/filters/CORSFilter.java
@@ -2,18 +2,31 @@ package lk.pos.filters;
 
 import java.io.IOException;
 
-
 /**
  * @author : A.D.Liyanage
  * @since : 0.1.0
  **/
 
-@WebServlet(urlPatterns = "/customerss")
-public class CustomerServlet extends HttpServlet {
+@WebFilter(urlPatterns = "/*")
+public class CORSFilter implements Filter {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-        System.out.println("request ");
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletResponse sr = (HttpServletResponse) servletResponse;
+        sr.addHeader("Access-Control-Allow-Origin","*");
+        sr.addHeader("Access-Control-Allow-Methods","DELETE,PUT");
+        sr.addHeader("Access-Control-Allow-Headers","Content-Type");
+        filterChain.doFilter(servletRequest, servletResponse);
+
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
-//http://localhost:8080/javaEEpos/customer
--- BackEnd_POS_System/src/servlet/CustomerServlet.java
+++ BackEnd_POS_System/src/servlet/CustomerServlet.java
@@ -1,9 +1,18 @@
 package servlet;
 
+import java.io.IOException;
+
 /**
  * @author : A.D.Liyanage
  * @since : 0.1.0
  **/
 
-public class CustomerServlet {
+@WebServlet(urlPatterns = "/customerss")
+public class CustomerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("request ");
+    }
 }
+//http://localhost:8080/javaEEpos/customer
